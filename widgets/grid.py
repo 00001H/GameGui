27acc00579557b936560c193b94fdb0a43e6fa89
00001H <Programmer00001H@gmail.com>
2022-12-10 22:46:14 +0800

    Update grid.py

--- widgets/grid.py
+++ widgets/grid.py
@@ -1,39 +1,8 @@
 """A 2D grid."""
 from ..bases import Widget
 from pygame.draw import rect
-class _Wrap:
-    """Wrapper to handle list/dict indexes."""
-    def __init__(self,dat):
-        self.NULL = (dat is None)
-        self.d = dat
-    def __getitem__(self,x):
-        if self.NULL:
-            return None
-        if (self.d.__class__ is dict)and(x not in self.d):
-            return None
-        return self.d[x]
-    def __setitem__(self,x,t):
-        self.d[x] = t
-    def inrange(self,x):
-        if (self.d.__class__ is dict):
-            return True
-        return x in range(len(self.d))
-class _GridData:
-    """Infinite 2D array."""
-    def __init__(self,w,h):
-        if w==None:
-            self.data = {}
-        else:
-            self.data = [({} if h is None else [None for j in range(h)])
-                         for i in range(w)]
-        self.w = w
-        self.h = h
-    def __getitem__(self,x):
-        if _Wrap(self.data)[x] is None:
-            self.data[x] = ({} if self.h is None else [None for j in range(h)])
-        return _Wrap(_Wrap(self.data)[x])
-    def inrange(self,x):
-        return _Wrap(self.data).inrange(x)
+def arr2(wx,wy):
+    return [[None for i in range(wy)] for i in range(wx)]
 class Grid(Widget):
     """2D grid.
 Arguments:
@@ -41,18 +10,18 @@ w: width of widget
 h: height of widget
 gridw: width of a single cell.
 gridh: height of a single cell.
-wgrids: width of the grid(unit:cells). If None, is infinite in width.
-hgrids: height of the grid(unit:cells). If None, is infinite in height.
+wgrids: width of the grid(unit:cells).
+hgrids: height of the grid(unit:cells).
 NOTE: Making a grid infinite may degrade performance.
 
 borderw: width of border(in pixels)(will cover cells)
 bordercolor: color of border
 """
-    def __init__(self,w,h,gridw,gridh,wgrids=None,hgrids=None,borderw=2,
+    def __init__(self,w,h,gridw,gridh,wgrids=10,hgrids=10,borderw=2,
                  bordercolor=(0,0,0)):
         self.width = w
         self.height = h
-        self.data = _GridData(wgrids,hgrids)
+        self.data = arr2(wgrids,hgrids)
         self.gw = gridw
         self.gh = gridh
         self.bw = borderw
@@ -75,10 +44,10 @@ bordercolor: color of border
             if da is not None:
                 blnk.blit(da.get_surface(),(co*gw,ro*gh))
             co += 1
-            if (co*gw)>wi:#out of screen
+            if (co*gw)>wi or co >= len(self.data):
                 co = 0
                 ro += 1
-                if (ro*gh)>he:#out of screen:done
+                if (ro*gh)>he or ro >= len(self.data[0]):
                     break
         #grid lines
         bw = self.bw
@@ -101,7 +70,7 @@ bordercolor: color of border
             pos = evt.pos
             x = pos[0]//self.gw
             y = pos[1]//self.gh
-            if (not self.data.inrange(x))or(not self.data[x].inrange(y))or(
+            if (x not in range(len(self.data)))or(y not in range(len(self.data[x])))or(
                 self.data[x][y] is None):
                 return False
             return self.data[x][y].handle_event(evt)
